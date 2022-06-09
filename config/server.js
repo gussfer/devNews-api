309331135ae65e3ffdd86578c2df4eb4987cf084
Gustavo Ferreira <oliveira.gustavo@unifesp.br>
2022-06-09 08:35:51 -0300

    try-fix

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('APP_HOST', 'https://dev-news-app.herokuapp.com/'),
+  host: env('APP_HOST', '172.20.10.4'),
   port: env.int('NODE_PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
